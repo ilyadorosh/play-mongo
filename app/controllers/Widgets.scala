b57030ba03e92b14c8971ba3eebfe8a313774148
ilyadorosh <ilyadorosh@gmail.com>
2016-10-16 22:58:47 +0300

    everything finally works

---
+++ app/controllers/Widgets.scala
@@ -0,0 +1,8 @@
+package controllers
+
+/**
+  * Created by james on 10/6/2016.
+  */
+class Widgets {
+
+}
---
+++ app/repos/WidgetRepo.scala
@@ -0,0 +1,8 @@
+package repos
+
+/**
+  * Created by james on 10/6/2016.
+  */
+class WidgetRepo {
+
+}
